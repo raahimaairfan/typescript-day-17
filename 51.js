5c3f657a6b5d4ce95e77b1369314869b3173272a
raahimaairfan <133574668+raahimaairfan@users.noreply.github.com>
2024-03-17 18:32:11 -0700

    Add files via upload

---
+++ 51.js
@@ -0,0 +1,10 @@
+/* Refactoring to Arrow Functions:
+Take a simple function that calculates the area of a rectangle and refactor it into an arrow function. */
+// Original function for calculating the area of a rectangle
+function calculateArea(width, height) {
+    return width * height;
+}
+// Refactored into an arrow function
+var calculateAreaArrow = function (width, height) { return width * height; };
+// Example usage of the arrow function
+console.log(calculateAreaArrow(5, 7)); // Logs the area of the rectangle
